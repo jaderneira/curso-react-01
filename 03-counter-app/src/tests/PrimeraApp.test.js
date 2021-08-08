683a13c7e9b56c5537c4039411bd8e9294c4aaca
Jader Neira <jneira@lean-tech.io>
2021-08-07 22:51:53 -0500

    ADD: PrimeraApp test

---
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import PrimeraApp from "../PrimeraApp";
+
+describe('Pruebas en <PrimeraApp />', () => {
+
+    test('debe mostrar el mensaje Hola soy Goku', () => {
+
+        const saludo = 'Hola soy Goku';
+        const { getByText } = render( <PrimeraApp saludo={saludo} /> );
+
+        expect( getByText(saludo) ).toBeInTheDocument();
+        
+    })
+  
+    
+})
+ 
