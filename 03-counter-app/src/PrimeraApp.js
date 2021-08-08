2d1c0062149efa871a2a275d6215f3be7ad784b2
Jader Neira <jneira@lean-tech.io>
2021-08-07 23:19:36 -0500

    ADD: tests with enzyme

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -5,9 +5,9 @@ const PrimeraApp = ({ saludo, subtitulo }) => {
     
     return (
         <>
-            <h1> { saludo } </h1>
+            <h1>{ saludo }</h1>
             {/* <pre>{ JSON.stringify(saludo , null, 4) }</pre> */}
-            <p> { subtitulo } </p>
+            <p>{ subtitulo }</p>
         </>
     );
      
--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme';
+
 import PrimeraApp from "../PrimeraApp";
 
 describe('Pruebas en <PrimeraApp />', () => {
 
-    test('debe mostrar el mensaje Hola soy Goku', () => {
+    // test('debe mostrar el mensaje Hola soy Goku', () => {
 
-        const saludo = 'Hola soy Goku';
-        const { getByText } = render( <PrimeraApp saludo={saludo} /> );
+    //     const saludo = 'Hola soy Goku';
+    //     const { getByText } = render( <PrimeraApp saludo={saludo} /> );
 
-        expect( getByText(saludo) ).toBeInTheDocument();
+    //     expect( getByText(saludo) ).toBeInTheDocument();
         
-    })
+    // })
   
+    test('debe mostrar <PrimeraApp /> correctamente ', () => {
+        const saludo = 'Hola soy Goku';
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>)
+
+        expect(wrapper).toMatchSnapshot(); 
+        
+    });
+
+    test('debe mostrar el subtitulo enviado por props', () => {
+        const saludo = 'Hola soy Goku';
+        const subTitulo = 'Soy el subtitulo nuevo';
+        const wrapper = shallow(
+            <PrimeraApp
+                saludo={ saludo }
+                subtitulo={ subTitulo }
+            />
+        );
+
+        const textoParrafo = wrapper.find('p').text();
+        
+        expect( textoParrafo ).toBe( subTitulo );
+    })
+    
+    
     
 })
  
